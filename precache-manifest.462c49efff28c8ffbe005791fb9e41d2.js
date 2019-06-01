01bc36c8d63df2957c5cdad523eca1f9dfae8e47
islavov <ivelin.slavov@gmail.com>
2019-06-01 15:55:10 +0200

    Updates

---
+++ precache-manifest.462c49efff28c8ffbe005791fb9e41d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab909f85e5c1fbad2df212c519bb57c",
+    "url": "/6lokavica/index.html"
+  },
+  {
+    "revision": "730812d2c3aab3e7281d",
+    "url": "/6lokavica/static/css/main.95a29905.chunk.css"
+  },
+  {
+    "revision": "856281628acf98f4f10c",
+    "url": "/6lokavica/static/js/2.93bfb7ad.chunk.js"
+  },
+  {
+    "revision": "730812d2c3aab3e7281d",
+    "url": "/6lokavica/static/js/main.923d5859.chunk.js"
+  },
+  {
+    "revision": "1eb1de6dec4d17edbfa4",
+    "url": "/6lokavica/static/js/runtime~main.eef6944c.js"
+  },
+  {
+    "revision": "570a3d4fa515976ad1a93f9554b9fb38",
+    "url": "/6lokavica/static/media/KIRILICATA.570a3d4f.png"
+  }
+]);
